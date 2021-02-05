e42eb4c3f6fbeac7e90e2eeae40ccb15fa456fd4
Anna <avnond@yandex.ru>
2021-02-05 20:39:34 +0300

    Изменения в функции возврата рандомного числа

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-console */
-let getRandomIntInclusive = function (min, max, sigh){ //sigh - кол-во знаков после запятой
+const getRandomIntInclusive = function (min, max, sigh){ //sigh - кол-во знаков после запятой
   min = Math.ceil(min);
   max = Math.floor(max);
   return (Math.random() * (max - min + 1) + min).toFixed(sigh);
 }
-console.log(getRandomIntInclusive(1, 300, 4));
+console.log(getRandomIntInclusive(1.1, 3.5, 4));
 // Источник :  https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
