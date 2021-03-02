06d89d0f08df6ce8a873e83a09d6581ba08145df
Anna <avnond@yandex.ru>
2021-03-02 14:42:27 +0300

    Задание 5

--- js/main.js
+++ js/main.js
@@ -25,6 +25,14 @@ import {
   COORDINATES_PRECISION
 } from './data.js';
 
+import {
+  addStringToElement,
+  defineHouseType,
+  createAvatarScr,
+  addChildWithTwoClasses,
+  addImgChildWithAttributes
+} from './generate-similar-elements.js';
+
 const getAd = () => {
   const ad = {
     author: {
@@ -51,7 +59,44 @@ const getAd = () => {
   return ad;
 }
 
+let newArray;
 const getArray = (length = 0, cb) => {
-  return Array.from({ length: length }, cb);
+  newArray= Array.from({ length: length }, cb);
+  return newArray;
 }
-getArray(10, getAd);
+console.log(getArray(10, getAd));
+
+// // Задание 5
+export let containerMap = document.getElementById('map-canvas');
+console.log(containerMap);
+let newCardTemplate = document.querySelector('#card').content;
+let cardAvatar = newCardTemplate.querySelector('.popup__avatar');
+let cardTitle = newCardTemplate.querySelector('.popup__title');
+let cardAddress = newCardTemplate.querySelector('.popup__text--address');
+let cardPrice = newCardTemplate.querySelector('.popup__text--price');
+let cardType = newCardTemplate.querySelector('.popup__type');
+let cardRoomsGuests = newCardTemplate.querySelector('.popup__text--capacity');
+let cardTime = newCardTemplate.querySelector('.popup__text--time');
+let cardFeatures = newCardTemplate.querySelector('.popup__features');
+let featureClasses = [
+  'popup__feature--wifi',
+  'popup__feature--dishwasher',
+  'popup__feature--parking',
+  'popup__feature--washer',
+  'popup__feature--elevator',
+  'popup__feature--conditioner',
+];
+
+let cardDescription = newCardTemplate.querySelector('.popup__description');
+let cardPhotos = newCardTemplate.querySelector('.popup__photos');
+
+createAvatarScr(newArray[0].author.avatar, cardAvatar);
+addStringToElement(newArray[0].offer.title, cardTitle);
+addStringToElement(newArray[0].offer.address, cardAddress);
+addStringToElement(newArray[0].offer.price + ' ₽/ночь', cardPrice);
+defineHouseType(newArray[0].offer.type, cardType);
+addStringToElement(newArray[0].offer.rooms + ' комнаты для ' + newArray[0].offer.guests + ' гостей', cardRoomsGuests);
+addStringToElement('Заезд после ' + newArray[0].offer.checkin + ' ,выезд до ' + newArray[0].offer.checkout, cardTime);
+addChildWithTwoClasses('li', 'popup__feature', featureClasses, cardFeatures, newArray[0].offer.features);
+addStringToElement(newArray[0].offer.description, cardDescription);
+addImgChildWithAttributes('img', '.popup-photo', cardPhotos, newArray[0].offer.photos);
