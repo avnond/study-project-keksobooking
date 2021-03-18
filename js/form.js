f88a7e23594f6bece6949e0ac92bea95255da931
Anna <avnond@yandex.ru>
2021-03-18 13:13:11 +0300

    задание 8.2

--- js/form.js
+++ js/form.js
@@ -11,6 +11,11 @@ export let adPriceInput = document.querySelector('#price');
 export let adTimeinInput = document.querySelector('#timein');
 export let adTimeoutInput = document.querySelector('#timeout');
 
+export let adRoomsInput = document.querySelector('#room_number');
+export let adCapacityInput = document.querySelector('#capacity');
+export let valuesOfRooms = adRoomsInput.querySelectorAll('option');
+export let valuesOfCapacity = adCapacityInput.querySelectorAll('option');
+
 export const defineValuesAndPrice = function (types) {
   if (adTypeInput.value === 'bungalow'){
     adPriceInput.setAttribute('placeholder', types.bungalow);
@@ -40,4 +45,38 @@ adTimeinInput.addEventListener('click', function() {
 
 adTimeoutInput.addEventListener('click', function() {
   adTimeinInput.value = adTimeoutInput.value;
+});
+
+export const defineRoomsAndCapacity = function () {
+  if (adRoomsInput.value === '1'){
+    for (let i = 0; i <valuesOfCapacity.length; i++){
+      valuesOfCapacity[i].setAttribute('disabled', 'disabled');
+    }
+    valuesOfCapacity[2].setAttribute('selected', 'selected')
+    valuesOfCapacity[2].removeAttribute('disabled', 'disabled');
+  }
+  if (adRoomsInput.value === '2'){
+    valuesOfCapacity[1].setAttribute('selected', 'selected');
+    valuesOfCapacity[0].setAttribute('disabled', 'disabled');
+    valuesOfCapacity[3].setAttribute('disabled', 'disabled');
+  }
+  if (adRoomsInput.value === '3'){
+    for (let i = 0; i <valuesOfCapacity.length; i++){
+      valuesOfCapacity[i].removeAttribute('selected', 'selected');
+      valuesOfCapacity[i].removeAttribute('disabled', 'disabled');
+    }
+    valuesOfCapacity[2].setAttribute('selected', 'selected');
+    valuesOfCapacity[3].setAttribute('disabled', 'disabled');
+  }
+  if (adRoomsInput.value === '100'){
+    for (let i = 0; i <valuesOfCapacity.length; i++){
+      valuesOfCapacity[i].setAttribute('disabled', 'disabled');
+    }
+    valuesOfCapacity[3].setAttribute('selected', 'selected');
+    valuesOfCapacity[3].removeAttribute('disabled', 'disabled');
+  }
+}
+
+adRoomsInput.addEventListener('click', function() {
+  defineRoomsAndCapacity();
 });
