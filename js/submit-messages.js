9a5ccc3e51338b232422af76dbc335fe21f42484
Anna <avnond@yandex.ru>
2021-03-25 18:38:14 +0300

    Задание 10

--- js/submit-messages.js
+++ js/submit-messages.js
@@ -1,21 +1,29 @@
 let main = document.querySelector('main');
 
-export let successSubmitMessage = document.querySelector('#success').content;
-console.log(successSubmitMessage);
+export let successSubmitMessage = document.querySelector('#success').content.querySelector('.success');
 export let errorSubmitMessage = document.querySelector('#error');
-console.log(errorSubmitMessage)
 
-export const showSubmitMessage = (element) => {
-  main.appendChild(element);
-
-  document.addEventListener('keydown', (evt) => {
-    if (evt.key === ('Escape' || 'Esc')) {
-      evt.preventDefault();
-      main.removeChild(main.lastChild);
-      console.log(main)
-      console.log('delete');
-    }
-  });
+const isEscEvent = (evt) => {
+  return evt.key === 'Escape' || evt.key === 'Esc' || evt.code === 27;
 };
 
+export const showSubmitMessage = (message) => {
+  main.appendChild(message);
 
+  const closeSuccessModal = () => {
+    message.remove();
+    document.removeEventListener('keydown', onPopupEscKeydown);
+  };
+
+  const onPopupEscKeydown = (evt) => {
+    if (isEscEvent(evt)) {
+      evt.preventDefault();
+      closeSuccessModal();
+    }
+  };
+
+  document.addEventListener('keydown', onPopupEscKeydown);
+  document.body.addEventListener('click', () => {
+    closeSuccessModal();
+  });
+};
